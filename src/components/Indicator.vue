47b80a0bf82b620ae742d00b4ed98c268db375e5
flyerjay <876173839@qq.com>
2017-03-08 17:20:59 +0800

    更新代码

---
+++ src/components/Indicator.vue
@@ -0,0 +1,19 @@
+<template>
+    <transition>
+        <div class="snowblog-indicator" v-show="visible">
+
+        </div>
+    </transition>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                visible:false,
+            }
+        }
+    }
+</script>
+<style lang="less" scoped>
+    
+</style>
